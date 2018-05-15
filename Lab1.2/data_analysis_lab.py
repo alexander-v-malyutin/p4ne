67198d07b5d5e2ae6dda174f41f5e36ea04da19e
Alexander Malyutin <av.malyutin@jet.msk.su>
2018-05-15 18:41:08 +0300

    ADD data_analysis_lab

--- Lab1.2/data_analysis_lab.py
+++ Lab1.2/data_analysis_lab.py
@@ -11,11 +11,11 @@ def getvalue(x):
 
 
 X = list(map(getvalue, sheet['A'][1:]))
-Y1 = list(map(getvalue, sheet['B'][1:]))
+#Y1 = list(map(getvalue, sheet['B'][1:]))
 Y2 = list(map(getvalue, sheet['C'][1:]))
 Y3 = list(map(getvalue, sheet['D'][1:]))
 
-pyplot.plot(X, Y1, label='Температура')
+#pyplot.plot(X, Y1, label='Температура')
 pyplot.plot(X, Y2, label='Отн. температура')
 pyplot.plot(X, Y3, label='Активность')
 #pyplot.plot(X, Y1, 'r--', X, Y2, X, Y3)
